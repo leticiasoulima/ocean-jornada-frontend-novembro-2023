c8bf7b84fb2a189c4b2f887a848b61d1f498be25
Leticia Souza Lima <leticia.souza.lima@alumni.usp.br>
2023-11-14 23:16:23 +0000

    Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import './App.css'
-import Card from './components/Card/Card'
+import Card from './components/Card/Card';
 
 function App() {
-  const item1={
+  const item1 = {
     name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
-  const item2={
+  const item2 = {
     name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags: ["Origem: Terra C-137"]
   }
 
-  const item3={
+  const item3 = {
     name: "Summer Smith",
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-
   }
-  
+
   const items = [item1, item2, item3]
 
   return (
     <>
-      {items.map(function (element) {
-        return <Card item={element} />
-      })}
+      <div className="cards">
+        {items.map(function (element) {
+          return <Card item={element} />
+        })}
+      </div>
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,17 @@
+import Tag from "../Tag/Tag"
+
 export default function Card(props) {
   const item = props.item
-  console.log(props)
-    return <div className="card">
-      <h2>{item.name}</h2>
-      <div className="tags">
-        <div className="tag">Status: Vivo</div>
-        <div className="tag">Espécie: Humana</div>
-        <div className="tag">Origem: Terra C-137</div>
-      </div>
-      <img src= {item.imageUrl} />
+
+  const tags = item.tags || []
+
+  return <div className="card">
+    <h2>{item.name}</h2>
+    <div className="tags">
+      {tags.map(function (element) {
+        return <Tag text={element} />
+      })}
     </div>
-  }
+    <img src={item.imageUrl} />
+  </div>
+}
