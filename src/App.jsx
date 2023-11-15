63bf0c8647a6e54662f53b350907f92a45363227
Leticia Souza Lima <leticia.souza.lima@alumni.usp.br>
2023-11-15 00:31:17 +0000

    Extração de código

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,37 @@ import './App.css'
 import Card from './components/Card/Card';
 
 function App() {
-  const item1 = {
-    name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
-  }
-
-  const item2 = {
-    name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    tags: ["Origem: Terra C-137"]
-  }
-
-  const item3 = {
-    name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-  }
-
-  //const items = [item1, item2, item3]
-
-  //useState
   const [items, setItems] = useState([])
 
   async function carregarDadosApi() {
     const apiUrl = "https://rickandmortyapi.com/api/character/"
-  
+
     const response = await fetch(apiUrl)
 
     const body = await response.json()
-    console.log(body);
 
-    setItems(body.results)
+    const results = body.results.map(function (element) {
+      return {
+        name: element.name,
+        image: element.image,
+        tags: [
+          `Status: ${element.status}`,
+          `Species: ${element.species}`,
+          `Origin: ${element.origin.name}`
+        ]
+      }
+    })
+
+    setItems(results)
   }
-  
-  useEffect(function () {
-  carregarDadosApi()
-  }, [])
 
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
 
   return (
     <>
-        <div className="cards">
+      <div className="cards">
         {items.map(function (element) {
           return <Card item={element} />
         })}
